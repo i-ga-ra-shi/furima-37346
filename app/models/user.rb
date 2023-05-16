8e9cd49699bd25f45ba6cc6fc8073e5053c36e4a
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-16 15:40:40 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :kanzi_last_name, :kanzi_first_name, :kana_last_name, :kana_first_name, :date_of_birth, presence:true
+
 end
--- db/migrate/20230515105249_devise_create_users.rb
+++ db/migrate/20230515105249_devise_create_users.rb
@@ -7,11 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :kanzi_last_name, null: false
-      t.string :kanzi_first_name, null: false
-      t.string :kana_last_name, null: false
-      t.string :kana_first_name, null: false
-      t.date   :date_of_birth, null: false
+      t.string :kanzi_last_name,    null: false
+      t.string :kanzi_first_name,   null: false
+      t.string :kana_last_name,     null: false
+      t.string :kana_first_name,    null: false
+      t.date   :date_of_birth,      null: false
 
       ## Recoverable
       t.string   :reset_password_token
