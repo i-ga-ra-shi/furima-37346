8e8a8e1d13ede52c56adc6b0af81916b6b5f6248
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-17 18:07:47 +0900

    itemsテーブルに商品の保存ができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    binding.pry
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :delivery_charge_id, :prefecture_id, :day_for_send_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :day_for_send_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
+    extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :user
     has_one_attached :image
+
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :delivery_charge
+    belongs_to :prefecture
+    belongs_to :day_for_send
+
+    validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :day_for_send_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+
 end
