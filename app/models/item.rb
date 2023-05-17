a3912dd5007bc2ca7dc78ec1cc23ed62da61499d
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-17 20:33:21 +0900

    バリデーションの設定とエラーメッセージの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
     belongs_to :day_for_send
 
     validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :day_for_send_id, numericality: { other_than: 1, message: "can't be blank" } 
-
-
+    validates :name, :explanation, :price, :image, presence: true
+    validates :price, format: { with: /\A\d+\z/, message: "is invalid. Input half-width characters" }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
