db0eedb1fe958a682372d356e10e83802ff7c11d
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-17 16:05:47 +0900

    newアクションとcreateアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :delivery_charge_id, :prefecture_id, :day_for_send_id, :price).merge(user_id: current_user.id)
   end
 end
