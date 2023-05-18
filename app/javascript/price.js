5f1cc958fb68045f9b29215d9fb62020cf59ed59
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-18 12:25:54 +0900

    javascript改良

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,12 +3,18 @@ function price() {
     ItemPrice.addEventListener('keyup', () => {
         const AddTaxPrice = document.getElementById("add-tax-price");
         const Profit = document.getElementById("profit");
-        const TaxNum = Math.floor(ItemPrice.value * 0.1).toLocaleString();
-        const ProfitNum = (parseInt(ItemPrice.value) - parseInt(TaxNum)).toLocaleString();
-        AddTaxPrice.innerHTML = `${TaxNum}`;
-        Profit.innerHTML = `${ProfitNum}`;
+        const PriceNum = parseFloat(ItemPrice.value);
+        const TaxNum = Math.floor(PriceNum * 0.1);
+        const ProfitNum = PriceNum - TaxNum;
+        
+        if (!/^[0-9]+$/.test(ItemPrice.value)) {
+            AddTaxPrice.innerHTML = "";
+            Profit.innerHTML = "";
+        }
+        if (!isNaN(ProfitNum) && !isNaN(TaxNum)) {
+            AddTaxPrice.innerHTML = `${TaxNum.toLocaleString(undefined, {useGrouping: true})}`;
+            Profit.innerHTML = `${ProfitNum.toLocaleString(undefined, {useGrouping: true})}`;
+        }
     })
 }
-
-
 window.addEventListener("load", price)
