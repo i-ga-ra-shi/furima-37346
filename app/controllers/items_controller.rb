00ebcbc82b3a644db8c0dde8f0244b47ce8c81cf
i-ga-ra-shi <kigararhi@gmail.com>
2023-05-23 21:32:56 +0900

    editアクションとupdateアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
-
+  
   def new
     @item = Item.new
   end
-
+  
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -17,19 +17,20 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @item = Item.find(params[:id])
   end
   
   def edit
     @item = Item.find(params[:id])
+    move_to_index
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end
@@ -37,6 +38,12 @@ class ItemsController < ApplicationController
 
 
   private
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
